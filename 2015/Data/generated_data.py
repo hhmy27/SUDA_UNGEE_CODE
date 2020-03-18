2ab1447f1a5ebe5a051e0e8ef997735019a28254
Arthur-Desktop <arthurren0803@gmail.com>
2020-03-18 18:34:01 +0800

    remove useless code

--- 2015/Data/generated_data.py
+++ 2015/Data/generated_data.py
@@ -1,8 +1,8 @@
 import random
 
 if __name__ == '__main__':
+    # 写入了200个[-30,30]的坐标点
     with open('input.dat', 'wb') as fb:
         for i in range(200):
-            num = random.randint(-100, 100)
-            print(num, end=' ')
+            num = random.randint(-30, 30)
             fb.write(num.to_bytes(4, byteorder="big", signed=True))
