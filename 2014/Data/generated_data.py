9fb655e9fa878e2ed9337e5705fe3cae3de74246
Arthur-Desktop <arthurren0803@gmail.com>
2020-03-19 17:43:31 +0800

    add new solution to 2014

--- 2014/Data/generated_data.py
+++ 2014/Data/generated_data.py
@@ -1,7 +1,8 @@
 import random
-import pickle
+
 if __name__ == '__main__':
     # 写入了200个[-30,30]的坐标点
     with open('input.dat','wb') as fb:
         for i in range(200):
-            pickle.dump(random.randint(-30,30),fb)
+            num = random.randint(-30,30)
+            fb.write(num.to_bytes(4, byteorder='big', signed=True))
