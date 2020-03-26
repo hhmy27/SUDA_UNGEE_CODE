01c5b6dbdc5a34e4fc67505c12ae86d349afaf4d
Arthur-Desktop <arthurren0803@gmail.com>
2020-03-26 17:58:09 +0800

    add linklist & unit test

---
+++ 初试/DataStructure/linklist.py
@@ -0,0 +1,58 @@
+from collections import Iterable
+
+
+class LinkNode(object):
+    """
+    Link Node of single link list
+    """
+
+    def __init__(self, value, nextNode=None):
+        self.__value = value
+        self.__next = nextNode
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        self.__value = value
+
+    @property
+    def next(self):
+        return self.__next
+
+    @next.setter
+    def next(self, nextNode):
+        self.__next = nextNode
+
+    def __iter__(self):
+        node = self
+        while node:
+            yield node
+            node = node.next
+
+    def __next__(self):
+        if self.next is None:
+            raise StopIteration()
+        else:
+            return self.next
+
+
+def createLinkList(values: Iterable) -> LinkNode:
+    """
+    Create a single link list without head node
+    :param values: values of the list
+    :return: first node of the list
+    """
+    assert isinstance(values, Iterable)
+    first = None
+    pre = None
+    for value in values:
+        node = LinkNode(value)
+        if first is None:
+            first = node
+        if pre is not None:
+            pre.next = node
+        pre = node
+    return first
